dcfdeed23ee4ad8a4e52fe0af3edc2e4949be8c6
avenir-crm <avenirindianks@gmail.com>
2025-07-10 22:59:16 +0530

    Update new

---
+++ loan.php
@@ -0,0 +1,17 @@
+<h2>Add Loan Details</h2>
+
+<form method="post" action="emi_calculation.php">
+    <label>Loan Amount:</label>
+    <input type="number" name="loan_amount" required><br><br>
+
+    <label>Interest Rate (per annum %):</label>
+    <input type="number" step="0.01" name="interest_rate" required><br><br>
+
+    <label>Loan Period (in months):</label>
+    <input type="number" name="period_months" required><br><br>
+
+    <label>Start Date:</label>
+    <input type="date" name="start_date" required><br><br>
+
+    <button type="submit" name="calculate">Calculate EMI</button>
+</form>

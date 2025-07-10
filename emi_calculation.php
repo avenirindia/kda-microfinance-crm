1ed60be44fd8a59681a50d3d4e61aeb84ad8443c
avenir-crm <avenirindianks@gmail.com>
2025-07-10 23:03:43 +0530

    new update

---
+++ emi_calculation.php
@@ -0,0 +1,50 @@
+<?php
+if(isset($_POST['calculate'])){
+    $loan_amount   = $_POST['loan_amount'];
+    $interest_rate = $_POST['interest_rate'];
+    $period_months = $_POST['period_months'];
+    $start_date    = $_POST['start_date'];
+
+    // EMI Formula: [P x R x (1+R)^N] / [(1+R)^N-1]
+    $monthly_interest_rate = ($interest_rate / 12) / 100;
+    $emi = ($loan_amount * $monthly_interest_rate * pow(1+$monthly_interest_rate, $period_months)) / 
+           (pow(1+$monthly_interest_rate, $period_months)-1);
+
+    $emi = round($emi, 2);
+    $total_payment = round($emi * $period_months, 2);
+    $total_interest = round($total_payment - $loan_amount, 2);
+
+    echo "<h2>EMI Calculation Result</h2>";
+    echo "Loan Amount: ₹".$loan_amount."<br>";
+    echo "Interest Rate: ".$interest_rate."%<br>";
+    echo "Loan Period: ".$period_months." months<br>";
+    echo "Start Date: ".$start_date."<br><br>";
+
+    echo "<strong>Monthly EMI: ₹".$emi."</strong><br>";
+    echo "Total Interest: ₹".$total_interest."<br>";
+    echo "Total Payment (Principal + Interest): ₹".$total_payment."<br><br>";
+
+    // কিস্তি শিডিউল
+    echo "<h3>EMI Schedule:</h3>";
+    echo "<table border='1' cellpadding='5'>
+        <tr>
+            <th>Installment No.</th>
+            <th>Payment Date</th>
+            <th>EMI Amount (₹)</th>
+        </tr>";
+
+    for($i=1; $i<=$period_months; $i++){
+        $payment_date = date('Y-m-d', strtotime("+$i month", strtotime($start_date)));
+        echo "<tr>
+            <td>$i</td>
+            <td>$payment_date</td>
+            <td>$emi</td>
+        </tr>";
+    }
+
+    echo "</table>";
+}
+else{
+    echo "Invalid Access!";
+}
+?>

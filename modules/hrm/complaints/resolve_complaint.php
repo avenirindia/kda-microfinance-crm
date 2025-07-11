3abb79caa9fec59741edb825e39357d74c1fc09c
avenir-crm <avenirindianks@gmail.com>
2025-07-11 17:23:06 +0530

    lol

--- modules/hrm/complaints/resolve_complaint.php
+++ modules/hrm/complaints/resolve_complaint.php
@@ -1,10 +1,1 @@
-<?php
-include '../config/db.php';
-
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $conn->query("UPDATE employee_complaints SET status='Resolved' WHERE id='$id'");
-    echo "<h3>✅ Complaint Resolved</h3>";
-    echo "<a href='admin_complaints.php'>Back to Complaints</a>";
-}
-?>
+include $_SERVER['DOCUMENT_ROOT']."/kda-microfinance-crm/config/db.php";

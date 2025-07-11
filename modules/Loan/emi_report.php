1655e452cce06b502b2325ad910b8a6a7e363d81
avenir-crm <avenirindianks@gmail.com>
2025-07-11 09:14:53 +0530

    ok

--- modules/Loan/emi_report.php
+++ modules/Loan/emi_report.php
@@ -6,3 +6,40 @@
     <input type="submit" value="Search">
 </form>
 <br>
+
+<h3>EMI Report</h3>
+
+<table border="1">
+<tr>
+    <th>EMI Date</th>
+    <th>Loan ID</th>
+    <th>Amount</th>
+</tr>
+
+<?php
+include 'dbconnect.php';
+
+$where = "WHERE 1"; 
+
+if(!empty($_GET['from_date'])){
+    $where .= " AND emi_date >= '".$_GET['from_date']."'";
+}
+if(!empty($_GET['to_date'])){
+    $where .= " AND emi_date <= '".$_GET['to_date']."'";
+}
+if(!empty($_GET['loan_id'])){
+    $where .= " AND loan_id = '".$_GET['loan_id']."'";
+}
+
+$query = "SELECT * FROM emi_table $where";
+
+$result = mysqli_query($conn, $query);
+while($row = mysqli_fetch_assoc($result)){
+    echo "<tr>";
+    echo "<td>".$row['emi_date']."</td>";
+    echo "<td>".$row['loan_id']."</td>";
+    echo "<td>".$row['amount']."</td>";
+    echo "</tr>";
+}
+?>
+</table>

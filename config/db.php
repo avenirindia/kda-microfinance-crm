0c73e31132bb4d8ff37b72c1580140034e91202f
avenir-crm <avenirindianks@gmail.com>
2025-07-11 20:31:25 +0530

    lela

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "kda_microfinance";
+$dbname = "kda_microfinance_crm";  // ← এইটা ঠিক করো
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {

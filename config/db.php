c1e8f869a2c91a776f9f51fb3ead5915f03dbcb4
avenir-crm <avenirindianks@gmail.com>
2025-07-11 22:43:35 +0530

    new

--- config/db.php
+++ config/db.php
@@ -1,5 +1,8 @@
 <?php
+// Database connection
 $conn = new mysqli("localhost", "root", "", "kda_microfinance_crm");
+
+// Connection check
 if($conn->connect_error){
     die("Database connection failed: " . $conn->connect_error);
 }

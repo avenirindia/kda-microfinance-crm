45ebeccd6b68532d70aca5db5307a9d2c8391ff8
avenir-crm <avenirindianks@gmail.com>
2025-07-11 19:55:03 +0530

    jaja

--- modules/hrm/emp_add.php
+++ modules/hrm/emp_add.php
@@ -1,4 +1,4 @@
-<?php include '../../../config/db.php'; ?>
+<?php include("../../config/db.php"); ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <div class="container mt-4">
